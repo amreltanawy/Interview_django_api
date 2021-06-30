8f545550c9e071782f85357f61bcd65a6462b788
tanawys <amraeltanawy@gmail.com>
2021-06-30 17:37:09 +0200

    add blueprint for predictor class to be used by django

---
+++ MLWrapper/api/Predictor.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+import pickle
+
+
+
+
+class Predictor():
+    def __init__(self):
+        pass
+        # self.model = pickle.load()
