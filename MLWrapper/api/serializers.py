2b04ab65ccd7652c16c44ea9c9aee66569e1cf7f
tanawys <amraeltanawy@gmail.com>
2021-07-02 12:29:23 +0200

    add prediction results serializer

--- MLWrapper/api/serializers.py
+++ MLWrapper/api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from api.models import FeatureSet
+from api.models import FeatureSet, PredictionResult
+
 
 
 class FeatureSetSerializer(serializers.ModelSerializer):
     class Meta:
         model = FeatureSet
+        fields = '__all__'
+
+class PredictionResultSerializer(serializers.ModelSerializer):
+    feature_set = FeatureSet
+    class Meta:
+        model = PredictionResult
         fields = '__all__'
